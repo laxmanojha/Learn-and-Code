3a48bb99b8af132b94f63a702c211cdffaccf8ae
Laxman Prasad Ojha <laxman.p.ojha@intimetec.com>
2025-04-13 16:13:26 +0530

    Question 1 completed and added

---
+++ Object and DS/Question1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee;
+            
+        /*
+        Is 'employee' an object or a data structure? Why?
+
+        Explaination: As per discussion in L&C I understood that here 'employee' is a data structure.
+        Why I am saying so because it does not have additional functionality but only getters and setters which indirectly exposes attributes or data to outer environment like data structures. 
+        */
+    }
+}
