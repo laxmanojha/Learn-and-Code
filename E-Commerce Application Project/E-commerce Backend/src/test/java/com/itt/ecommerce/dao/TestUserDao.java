c4bbdf008d51e2a3f7d38d53d2474bfb828cfc13
Laxman Prasad Ojha <laxman.p.ojha@intimetec.com>
2025-05-17 23:02:59 +0530

    approach to run the controller test cases

--- E-Commerce Application Project/E-commerce Backend/src/test/java/com/itt/ecommerce/dao/TestUserDao.java
+++ E-Commerce Application Project/E-commerce Backend/src/test/java/com/itt/ecommerce/dao/TestUserDao.java
@@ -1,5 +1,5 @@
 package com.itt.ecommerce.dao;
 
 public class TestUserDao {
-
+	// To Run the controller test I have move the Server from modulepath to class path and all the WEB-INF lib to classpath.
 }
